6fe0cadabc1bc03c5aac12225117601526e2ed6a
Sean Perkins <sean@mobility-labs.com>
2017-07-14 17:31:18 -0400

    Update home

--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,9 @@ export default class Home extends Component {
     return (
       <div>
         <h1>
-          <small>Hi Joell! You visited us recently</small>
-          We need your help to continually improve our hospital expierence
+          We put this together because we had an unpleasant experience with a post hospital visit survey.
         </h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>We are hoping to use this as a prototype of what a better experience could be.</p>
         <Link to="/steps/1" className="button">Let's get started</Link>
       </div>
     );
